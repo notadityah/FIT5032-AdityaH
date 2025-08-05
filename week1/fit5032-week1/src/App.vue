e8b3879739a4ff0406f7c5f5ef6f4bb35d1f133b
Aditya Hariharan <aditya12320@gmail.com>
2025-08-05 17:19:55 +1000

    week 1 lab

---
+++ week1/fit5032-week1/src/App.vue
@@ -0,0 +1,59 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Counter from './components/Counter.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="My name is Adi" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+main {
+  margin: 14px;
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+</style>
---
+++ week1/fit5032-week1/src/components/Counter.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+</script>
+
+<style scoped>
+button {
+  padding: 12px 12px;
+  font-size: 16px;
+  cursor: pointer;
+  background-color: #181818;
+  color: rgb(235, 235, 235, 0.64);
+  border: none;
+  border-radius: 5px;
+}
+</style>
+
+<template>
+  <button @click="increment">{{ count }}</button>
+</template>
